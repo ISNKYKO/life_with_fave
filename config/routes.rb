79ea190ca74dac1e11d20e3823ab2b3371d37012
ISNKYKO <164849442+ISNKYKO@>
2024-07-10 13:44:21 +0000

    activeadminを戻した

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   
   namespace :admin do
     get 'dashboards', to: 'dashboards#index'
-    resources :users, only: [:destroy]
-  end
+    resources :users, only: [:destroy] 
+ end
+
   
   scope module: :public do
   devise_for :users, controllers: {
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_07_08_144144) do
+ActiveRecord::Schema.define(version: 2024_07_09_151347) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -98,10 +98,10 @@ ActiveRecord::Schema.define(version: 2024_07_08_144144) do
     t.boolean "is_active", default: true
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "deactivated"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
